e70d10ebc707f298392da510f23c378f093cd372
Gabor Farkas <farkas.gz@gmail.com>
2025-09-04 16:41:56 +0200

    added criteria for user signup

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const bcryptjs = require("bcryptjs");
 const saltRounds = 12;
 
+const mongoose = require("mongoose");
+
 const User = require("../models/User.model");
 
 router.get("/signup", (req, res, next) => {
@@ -12,6 +14,23 @@ router.get("/signup", (req, res, next) => {
 
 router.post("/signup", (req, res, next) => {
   const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    res.render("auth/signup", { errorMessage: "All fields are required" });
+    return;
+  }
+
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res
+      .status(500)
+      .render("auth/signup", {
+        errorMessage:
+          "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
+      });
+    return;
+  }
+
   bcryptjs
     .genSalt(saltRounds)
     .then((salt) => bcryptjs.hash(password, salt))
@@ -25,7 +44,17 @@ router.post("/signup", (req, res, next) => {
     .then((userFromDB) => {
       res.redirect("/userProfile");
     })
-    .catch((error) => next(error));
+    .catch((error) => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(500).render("auth/signup", { errorMessage: error.message });
+      } else if (error.code === 11000) {
+        res.status(500).render("auth/signup", {
+          errorMessage: "Either username or email is already used.",
+        });
+      } else {
+        next(error);
+      }
+    });
 });
 
 router.get("/userProfile", (req, res) => res.render("users/user-profile"));
